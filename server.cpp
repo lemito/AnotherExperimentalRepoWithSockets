da7221126531fad1f4d2dadc9efb1d9c259adf4a
Michael Tropin <mihtrop2005@yandex.ru>
2025-01-19 13:17:17 +0300

    проверочки, bind и listen

--- server.cpp
+++ server.cpp
@@ -32,9 +32,25 @@ class server {
                       SOCK_STREAM,  // TCP
                       0             // default
     );
+    if (__sockfd < 0) {
+      std::cerr << "Error creating socket" << std::endl;
+      throw;
+    } else {
+      std::cout << "Socket created" << std::endl;
+    }
     __address.sin_family = AF_INET;
     __address.sin_addr.s_addr = inet_addr(adress.c_str());
     __address.sin_port = htons(port);
+
+    if (-1 == bind(__sockfd, (sockaddr*)&__address, sizeof(__address))) {
+      std::cerr << "Error binding socket and address" << std::endl;
+      throw;
+    }
+
+    if (-1 == listen(__sockfd, SOMAXCONN)) {
+      std::cerr << "Error listening socket with SOMAXCONN = " << SOMAXCONN << std::endl;
+      throw;
+    }
   };
   ~server() {
 #ifdef _WIN32
