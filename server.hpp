2d3dbd756d65e88f917c9f5315530ec013298e2c
Michael lemito <delovoy.lemit@gmail.com>
2025-01-21 22:17:15 +0300

    fix

--- server.hpp
+++ server.hpp
@@ -99,12 +99,14 @@ class server {
   void sendFromBuffer(const char* buf) const {}
   template <size_t buf_siz>
   void readToBuffer(char (*buf)[buf_siz]) const {
+    int status;
     if (-1 == (status = read(_conn, buf, buf_siz))) {
       throw;
     }
   }
   template <size_t buf_siz>
   void sendFromBuffer(char const (*buf)[buf_siz]) const {
+    int status;
     if (-1 == (status = send(_conn, buf, buf_siz, 0))) {
       throw;
     }
